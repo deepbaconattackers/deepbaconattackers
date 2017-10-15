cb5d7e3c9e1c9a2ce409761db03e3a6c9ba12aaa
Josiah <spacetagman@gmail.com>
2017-10-14 23:57:24 -0500

    Bootstrap!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+        staticFiles.location("/public");
         port(getHerokuAssignedPort());
 
         get("/hello", (req, res) -> "<p>(SPIDER) Why hello Meester Ant!</p>\n(ANT) H-hello?\n(SPIDER) It's so nice to see you tooday!\n(ANT) I-it is?\n(SPIDER) Why yes! In fact, I would luv it eef you could join me FOR DEENUR!!! BWAHAHAHAHA!!!\n");
