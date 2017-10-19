9d99a43a7c9a9a3d01dd4147bea950edd64e03f0
Jacob.Barna <beavis69>
2017-10-18 22:31:50 -0500

    got rid of lombok code that didn't work with heroku

--- src/main/java/app/util/Path.java
+++ src/main/java/app/util/Path.java
@@ -8,9 +8,28 @@ public class Path {
     // the variables from Velocity Templates
     public static class Web {
         //@Getter public static final String INDEX = "/index/";
-        @Getter public static final String LOGIN = "/login/";
-        @Getter public static final String LOGOUT = "/logout/";
-        @Getter public static final String INDEX = "/index/";
+//        @Getter public static final String LOGIN = "/login/";
+//        @Getter public static final String LOGOUT = "/logout/";
+//        @Getter public static final String INDEX = "/index/";
+
+        public static final String LOGIN = "/login/";
+        public static final String LOGOUT = "/logout/";
+        public static final String INDEX = "/index/";
+
+        public static String getINDEX()
+        {
+            return INDEX;
+        }
+
+        public static String getLOGIN()
+        {
+            return LOGIN;
+        }
+
+        public static String getLOGOUT()
+        {
+            return LOGOUT;
+        }
     }
 
     public static class Template {
