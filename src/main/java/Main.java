f434b5bcc9b4dce7a9025bbb188c97c4fc7a1146
Jason Stratman <jpstratman@unomaha.edu>
2017-10-18 20:50:34 -0500

    Commented out login functionality, re-added Heroku port check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Configure Spark
-        port(4567);
+        port(getHerokuAssignedPort());
 
         staticFiles.location("/public");
 
@@ -33,6 +33,14 @@ public class Main {
         after("*",                   Filters.addGzipHeader);
 
     }
+
+    static int getHerokuAssignedPort() {
+      ProcessBuilder processBuilder = new ProcessBuilder();
+      if (processBuilder.environment().get("PORT") != null) {
+          return Integer.parseInt(processBuilder.environment().get("PORT"));
+      }
+      return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+  }
 
 }
 
