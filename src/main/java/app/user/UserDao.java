7ffb91975bf3aaeb2555bac5f27524060a71654f
Anna Buhman <buhmana@gmail.com>
2017-10-23 16:17:02 -0500

    typo

--- src/main/java/app/user/UserDao.java
+++ src/main/java/app/user/UserDao.java
@@ -9,7 +9,7 @@ public class UserDao {
     //        Username    Salt for hash                    Hashed password (the password is "password" for all users)
     private final List<User> users = ImmutableList.of(
             new User("jbarna", "$2a$10$h.dl5J86rGH7I8bD9bZeZe", "$2a$10$h.dl5J86rGH7I8bD9bZeZeci0pDt0.VwFTGujlnEaZXPf/q7vM5wO"),
-            new User("abhuman",  "$2a$10$e0MYzXyjpJS7Pd0RVvHwHe", "$2a$10$e0MYzXyjpJS7Pd0RVvHwHe1HlCS4bZJ18JuywdEMLT83E1KDmUhCy"),
+            new User("abuhman",  "$2a$10$e0MYzXyjpJS7Pd0RVvHwHe", "$2a$10$e0MYzXyjpJS7Pd0RVvHwHe1HlCS4bZJ18JuywdEMLT83E1KDmUhCy"),
             new User("jpstratman",  "$2a$10$E3DgchtVry3qlYlzJCsyxe", "$2a$10$E3DgchtVry3qlYlzJCsyxeSK0fftK4v0ynetVCuDdxGVl1obL.ln2"),
             new User("jkrutz",  "$2a$10$E3DgchtVry3qlYlzJCsyxe", "$2a$10$E3DgchtVry3qlYlzJCsyxeSK0fftK4v0ynetVCuDdxGVl1obL.ln2")
     );
